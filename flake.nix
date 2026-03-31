fc055be8b47faec36b6432daee68cc7fdc81c932
kasader <casada980@gmail.com>
2026-03-31 22:08:32 +0900

    Bump home-manager version to 25.11

--- flake.nix
+++ flake.nix
@@ -2,10 +2,10 @@
 	description = "My Nix home-manager configuration";
 
 	inputs = {
-		nixpkgs.url = "nixpkgs/nixos-23.11";
+		nixpkgs.url = "nixpkgs/nixos-25.11";
 
 		home-manager  = {
-			url = "github:nix-community/home-manager/release-23.11";
+			url = "github:nix-community/home-manager/release-25.11";
 			inputs.nixpkgs.follows = "nixpkgs"; # Stating *.follows = 'nixpkgs'; indicates that
 			# the home-manager input is DEPENDENT on nixpkgs. I.e, an import DAG creation.
 		};
@@ -18,7 +18,7 @@
 			pkgsLinux = import nixpkgs {
 				system = "x86_64-linux";
 				config.allowUnfree = true;
-      		};
+      };
 
 			pkgsDarwin = import nixpkgs {
 				system = "aarch64-darwin";
--- modules/shared.nix
+++ modules/shared.nix
@@ -1,52 +1,73 @@
-{ lib, pkgs, ... }: let
-	username = "kasada-123";
-in {
-	programs.home-manager.enable = true;
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  username = "kasada-123";
+in
+{
+  programs.home-manager.enable = true;
 
-	imports = [
-		./fzf.nix
-		./ripgrep.nix
-		./vim.nix
-		./starship/default.nix
-		./tmux/default.nix
-		./yazi/default.nix
-		./fish/default.nix 
-		./git/default.nix
-		./bash/default.nix
-	];
+  imports = [
+    ./fzf.nix
+    ./ripgrep.nix
+    ./vim.nix
+    ./starship/default.nix
+    ./tmux/default.nix
+    ./yazi/default.nix
+    ./fish/default.nix
+    ./git/default.nix
+    ./bash/default.nix
+  ];
 
-	home = {
-		packages = with pkgs; [
-			cowsay
-			lolcat
-			bat
-			delta
-			htop
-			fortune
-			jq
-			nixfmt
+  home = {
+    packages = with pkgs; [
+      cowsay
+      lolcat
+      bat
+      pay-respects
+      delta
+      htop
+      fortune
+      jq
+      nixfmt
       go # just for testing (for now)
-		];
+      direnv
+    ];
 
-		file = {
-			"hello.txt" = {
-				text = ''
-					#!/usr/bin/env bash
+    file = {
+      "hello.txt" = {
+        text = ''
+          					#!/usr/bin/env bash
 
-					echo "Hello, ${username}!"
-					echo '*slaps roof* This script can fit so many lines in it'
-				'';
-				executable = true;
-			};
-		};
+          					echo "Hello, ${username}, @ ${config.home.homeDirectory}!"
+          					echo '*slaps roof* This script can fit so many lines in it'
+          				'';
+        executable = true;
+      };
+    };
 
-		inherit username;
-		homeDirectory_test = "/home/${username}";
+    # This value determines the Home Manager release that
+    # this configuration is compatible with. It helps to
+    # avoid breakage when a new Home Manage release
+    # introduces backwards incompatible changes.
+    stateVersion = "25.11";
+  };
 
-		# This value determines the Home Manager release that
-		# this configuration is compatible with. It helps to
-		# avoid breakage when a new Home Manage release
-		# introduces backwards incompatible changes.
-		stateVersion = "23.11";
-	};
+  # TODO: Move this to its own standalone file (if it is needed).
+  programs.direnv = {
+    enable = true;
+    # FIXME: It seems like if you have 'enable = true;' then you don't need to configure your
+    # options per shell.
+    # enableFishIntegration = true;
+    nix-direnv.enable = true;
+    config = {
+      global = {
+        hide_env_diff = true;
+      };
+    };
+  };
+
 }
